3590ef68970d6dc5ec6807aa02ef283eda16c288
sdeix <sdeix2765@gmail.com>
2023-02-05 21:01:01 +0700

    Шестое задание

--- js/main.js
+++ js/main.js
@@ -6,36 +6,26 @@ let app = new Vue({
        altText: "A pair of socks",
        inStock: true,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-variants: [
-   {
-       variantId: 2234,
-       variantColor: 'green',
-       variantImage: "./assets/vmSocks-green-onWhite.jpg",
-   },
-   {
-       variantId: 2235,
-       variantColor: 'blue',
-       variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-   }
-],
-
-       cart: 0,
-
+       variants: [
+           {
+               variantId: 2234,
+               variantColor: 'green',
+               variantImage: "./assets/vmSocks-green-onWhite.jpg",
+           },
+           {
+               variantId: 2235,
+               variantColor: 'blue',
+               variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+           }
+       ],
+       cart: 0
    },
    methods: {
-   addToCart() {
-       this.cart += 1
-   },
-   removeFromCart() {
-   if(this.cart>0){
-   this.cart -= 1
+       addToCart() {
+           this.cart += 1
+       },
+       updateProduct(variantImage) {
+           this.image = variantImage
+       }
    }
-
-   },
-   updateProduct(variantImage) {
-   this.image = variantImage},
-
-}
-
-
 })
